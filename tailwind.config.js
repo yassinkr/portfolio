5fb7f705b5278ff64a959a65cf2bd86593c8570a
yassinkr <kerraiyassin@gmail.com>
2024-04-02 23:48:27 +0100

    made it responsive again 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
 
     extend: {
       screens: {
-        'xs':'400px ',
+        'xxs':'300px',
+        'xs':'500px ',
         'sm': '640px',
         'md': '768px',
         'lg': '1024px',
